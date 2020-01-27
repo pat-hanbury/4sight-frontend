24e205d2ef6d9ebdfd10ef4263192b604d1b8ff3
Chris Barth <barth.c@husky.neu.edu>
2020-01-27 16:24:12 -0500

    added the first components

---
+++ app/src/app/feed-view/feed-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-view',
+  templateUrl: './feed-view.component.html',
+  styleUrls: ['./feed-view.component.css']
+})
+export class FeedViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ app/src/app/stat-view/stat-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-view',
+  templateUrl: './stat-view.component.html',
+  styleUrls: ['./stat-view.component.css']
+})
+export class StatViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
