9ef3179eea53df8302d7f076cdee7fb5bbab3eb7
Chris Barth <barth.c@husky.neu.edu>
2020-01-29 14:52:53 -0500

    first demo of data showed

--- app/src/app/dashboard/dashboard.component.ts
+++ app/src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  areas: Area[] = [
+    {id: 1, name: "area 1", img: "image to display"}, 
+    {id: 2, name: "area 2", img: "image to display"}, 
+    {id: 3, name: "area 3", img: "image to display"}, 
+    {id: 4, name: "area 4", img: "image to display"}, 
+    {id: 5, name: "area 5", img: "image to display"}, 
+  ];
+
+  selectedArea: Area;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(area) {
+    console.log(area);
+    this.selectedArea = area;
+  }
+
+}
+
+
+export class Area {
+  id: number;
+  name: string;
+  img: string;
 }
